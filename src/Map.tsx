ecf88af773df4751a6272c42bc30ccc4a69d995f
Albert Sanchez <albert.san.iza@gmail.com>
2025-04-27 15:41:32 -0700

    refactor: enhance Map component with state management and dynamic fill colors

--- src/Map.tsx
+++ src/Map.tsx
@@ -4,12 +4,15 @@ import { useEffect, useRef, useState } from 'react'
 import { feature } from 'topojson-client'
 import { Topology } from 'topojson-specification'
 
+import { useGameStore } from './lib/store'
+
 const WIDTH = 1000
 const HEIGHT = 500
 
 export default function Map() {
     const ref = useRef<SVGSVGElement>(null)
     const [geoJson, setGeoJson] = useState<FeatureCollection>()
+    const { started, finished, answeredStates, answerState, setStates } = useGameStore()
 
     useEffect(() => {
         fetch('https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json')
@@ -18,6 +21,7 @@ export default function Map() {
                 const gj = feature(topology, topology.objects.states)
                 if (gj.type === 'FeatureCollection') {
                     setGeoJson(gj)
+                    setStates(gj.features.map((feature) => feature.properties?.name || '').filter(Boolean))
                 }
             })
     }, [])
@@ -35,10 +39,14 @@ export default function Map() {
             .data(geoJson.features)
             .join('path')
             .attr('d', path)
-            .attr('fill', 'black')
+            .attr('fill', (data) =>
+                answeredStates[data.properties?.name] === 'correct' ? 'green' : answeredStates[data.properties?.name] === 'incorrect' ? 'red' : 'black'
+            )
             .attr('stroke', 'white')
             .attr('stroke-width', 0.5)
-    }, [geoJson])
+            .style('cursor', started && !finished ? 'pointer' : 'default')
+            .on('click', (_, data) => answerState(data.properties?.name))
+    }, [geoJson, started, finished, answeredStates, answerState])
 
     return <svg className="size-full" ref={ref} viewBox={`0 0 ${WIDTH} ${HEIGHT}`} />
 }
