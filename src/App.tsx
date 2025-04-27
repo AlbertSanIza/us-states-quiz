6771ab47e645b4bc921ebec189cadcbe0127ecd1
Albert Sanchez <albert.san.iza@gmail.com>
2025-04-27 14:19:27 -0700

    feat: update App component layout and content for US States Quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
-    return <main className="bg-red-500">Hello</main>
+    return (
+        <main className="absolute inset-0 flex flex-col gap-6 p-6">
+            <h1 className="text-4xl font-bold tracking-tight">US States Quiz</h1>
+            <div className="flex-1 border">The Map</div>
+        </main>
+    )
 }
