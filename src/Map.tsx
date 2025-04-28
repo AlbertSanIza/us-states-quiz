32be1904c1bbc81ea6a02aeee7954fc4ee4f77ed
Albert Sanchez <albert.san.iza@gmail.com>
2025-04-27 18:30:03 -0700

    refactor: update fill and stroke attributes for map paths to enhance visual clarity

--- src/Map.tsx
+++ src/Map.tsx
@@ -38,25 +38,27 @@ export default function Map() {
             .data(geoJson.features)
             .join('path')
             .attr('d', path)
-            .attr('fill', (data) =>
-                answered[data.properties?.name] === 'correct'
-                    ? 'oklch(72.3% 0.219 149.579)' // Green
-                    : answered[data.properties?.name] === 'incorrect'
-                      ? 'oklch(57.7% 0.245 27.325)' // Red
-                      : 'black'
+            .attr(
+                'fill',
+                (data) =>
+                    answered[data.properties?.name] === 'correct'
+                        ? 'oklch(72.3% 0.219 149.579)' // Green
+                        : answered[data.properties?.name] === 'incorrect'
+                          ? 'oklch(57.7% 0.245 27.325)' // Red
+                          : 'oklch(96.7% 0.001 286.375)' // Zinc 100
             )
-            .attr('stroke', 'white')
-            .attr('stroke-width', 0.5)
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
             .style('cursor', (data) => (started && !finished && !answered[data.properties?.name] ? 'pointer' : 'default'))
             .on('click', (_, data) => !answered[data.properties?.name] && answer(data.properties?.name))
             .on('mouseover', function (_, data) {
                 if (started && !finished && !answered[data.properties?.name]) {
-                    select(this).attr('fill', 'oklch(27.8% 0.033 256.848)') // Gray
+                    select(this).attr('fill', 'black')
                 }
             })
             .on('mouseout', function (_, data) {
                 if (started && !finished && !answered[data.properties?.name]) {
-                    select(this).attr('fill', 'black')
+                    select(this).attr('fill', 'oklch(96.7% 0.001 286.375)') // Zinc 100
                 }
             })
     }, [geoJson, started, finished, answered, answer])
