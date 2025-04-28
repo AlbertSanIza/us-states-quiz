42f93fea8ba32260dfa456f1c79bd7ee01b1453a
Albert Sanchez <albert.san.iza@gmail.com>
2025-04-27 17:17:34 -0700

    fix: update fill colors in Map component for better visibility

--- src/Map.tsx
+++ src/Map.tsx
@@ -40,7 +40,11 @@ export default function Map() {
             .join('path')
             .attr('d', path)
             .attr('fill', (data) =>
-                answeredStates[data.properties?.name] === 'correct' ? 'green' : answeredStates[data.properties?.name] === 'incorrect' ? 'red' : 'black'
+                answeredStates[data.properties?.name] === 'correct'
+                    ? 'oklch(62.7% 0.194 149.214)' // Green
+                    : answeredStates[data.properties?.name] === 'incorrect'
+                      ? 'oklch(57.7% 0.245 27.325)' // Red
+                      : 'black'
             )
             .attr('stroke', 'white')
             .attr('stroke-width', 0.5)
